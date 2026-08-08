6eea19a49b9002f0cc9c11eff320e915ef10349d
HELLnyk <yurock.ua@gmail.com>
2026-08-08 11:10:15 +0300

    Initial commit

---
+++ user-service/src/main/java/ua/vg/msg/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.vg.msg.userservice;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * UserServiceApplication — TODO.
+ *
+ * @author ykalapusha
+ * @since 08.08.2026
+ */
+@SpringBootApplication
+public class UserServiceApplication {
+}
