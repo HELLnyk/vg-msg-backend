2ec57b13895c3dd62f8d1437e791975b6e4f2e87
HELLnyk <yurock.ua@gmail.com>
2026-08-09 18:58:17 +0300

    init commit for messaging service

---
+++ message-service/src/main/java/ua/vg/msg/messageservice/MessageServiceApplication.java
@@ -0,0 +1,18 @@
+package ua.vg.msg.messageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * MessageServiceApplication — TODO.
+ *
+ * @author ykalapusha
+ * @since 09.08.2026
+ */
+@SpringBootApplication
+public class MessageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageServiceApplication.class, args);
+    }
+}
