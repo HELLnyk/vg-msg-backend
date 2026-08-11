68171d5a8c8a7bcc7ec7d99ecb83242e35400797
HELLnyk <yurock.ua@gmail.com>
2026-08-11 11:44:26 +0300

    refactoring regis cache for users

--- user-service/src/main/java/ua/vg/msg/userservice/config/RedisCacheConfig.java
+++ user-service/src/main/java/ua/vg/msg/userservice/config/RedisCacheConfig.java
@@ -6,8 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
+import ua.vg.msg.userservice.dto.user.UserDetailResponse;
+import ua.vg.msg.userservice.dto.user.UserResponse;
 
 import java.time.Duration;
 import java.util.Map;
@@ -17,26 +19,27 @@ public class RedisCacheConfig {
 
     @Bean
     CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-        RedisCacheConfiguration defaults = defaultConfiguration();
+        RedisCacheConfiguration defaults = RedisCacheConfiguration.defaultCacheConfig()
+                .disableCachingNullValues();
 
         Map<String, RedisCacheConfiguration> cacheConfigurations = Map.of(
-                "user", defaults.entryTtl(Duration.ofMinutes(15)),
+                "user", defaults.entryTtl(Duration.ofMinutes(15))
+                        .serializeValuesWith(
+                                RedisSerializationContext.SerializationPair.fromSerializer(
+                                        new JacksonJsonRedisSerializer<>(UserResponse.class)
+                                )
+                        ),
                 "userDetails", defaults.entryTtl(Duration.ofMinutes(5))
+                        .serializeValuesWith(
+                                RedisSerializationContext.SerializationPair.fromSerializer(
+                                        new JacksonJsonRedisSerializer<>(UserDetailResponse.class)
+                                )
+                        )
         );
 
         return RedisCacheManager.builder(connectionFactory)
                 .cacheDefaults(defaults)
                 .withInitialCacheConfigurations(cacheConfigurations)
                 .build();
-    }
-
-    private RedisCacheConfiguration defaultConfiguration() {
-        return RedisCacheConfiguration.defaultCacheConfig()
-                .disableCachingNullValues()
-                .serializeValuesWith(
-                        RedisSerializationContext.SerializationPair.fromSerializer(
-                                new GenericJackson2JsonRedisSerializer()
-                        )
-                );
     }
 }
